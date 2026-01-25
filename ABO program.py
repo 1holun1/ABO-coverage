5546142bb9c64ae96c3cb2228c9fcb5daa6a9370
1holun1 <1holun1@gmail.com>
2026-01-25 18:39:28 +0800

    Update ABO program.py

--- ABO program.py
+++ ABO program.py
@@ -116,7 +116,8 @@ if not df.empty:
                 # FIXED STRING LITERAL HERE
                 res_df = pd.DataFrame({
                     "Antibiotic": coverage.index,
-                    "Coverage": coverage.values
+                    "Coverage": coverage.values,
+                     hide_index=True
                 })
 
                 def highlight_tab2(val):
@@ -135,6 +136,7 @@ if not df.empty:
 with st.sidebar:
     st.write("### Legend")
     st.info("**Green (✔)**: Susceptible\n\n**Yellow (V)**: Variable \n\n**Gray**: No data/ Resistant \n\n https://docs.google.com/spreadsheets/d/1Xso78JWtiMLXKZK9-25GuDxohZLO55oe/edit?usp=sharing&ouid=105111822552474365895&rtpof=true&sd=true")
+
 
 
 
